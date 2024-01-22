0c1958c718d9571be2a9565461e921ccb14e3315
ArmaanTechDeveloper <kournarinder948@gmail.com>
2024-01-22 21:13:19 +0530

    sending html as response and responding to different urls also setting the statuscode

--- HTTP Server/index.js
+++ HTTP Server/index.js
@@ -3,14 +3,34 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req , res) => {
-    res.writeHead(200 , {
-        'Content-Type' : 'application/json'
-    });
 
-    res.end(JSON.stringify({
-        id: 1,
-        message: 'Hello world'
-    }))
+    if(req.url === '/hello'){
+        res.writeHead(200 , {
+            'Content-Type' : 'application/json'
+        });
+    
+        res.end(JSON.stringify({
+            id: 1,
+            message: 'Hello world'
+        }))
+    }
+    else if (req.url === '/messages') {
+        res.statusCode = 200; // optional to add it will be automatically 200 if nothing is supplied
+        res.setHeader('Content-Type' , 'text/html');
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<ul>')
+        res.write('<li> Hello this is a html file')
+        res.write('<li> This is another mesage from html file')
+        res.write('</ul>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
+    }
+    else{
+        res.statusCode = 404;
+        res.end();
+    }
 
 })
 
