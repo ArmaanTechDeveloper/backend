015ed335424f467161d62cc87b7be6289638ff84
ArmaanTechDeveloper <kournarinder948@gmail.com>
2024-02-13 01:38:40 +0530

    completed the authentication and authorization with google functionality with node security

--- Security-example/server.js
+++ Security-example/server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const helmet = require('helmet')
 const passport = require('passport')
 const { Strategy } = require('passport-google-oauth20')
+const cookieSession = require('cookie-session')
 require('dotenv').config()
 
 const logger = require('./utils/logger')
@@ -13,7 +14,8 @@ const app = express();
 
 const config = {
     clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
+    clientSecret: process.env.CLIENT_SECRET,
+    cookieKey: process.env.COOKIE_KEY
 }
 
 const authOptions = {
@@ -23,21 +25,39 @@ const authOptions = {
 }
 
 function verifyCallback(accessToken , refreshToken , profile , done) {
-    console.log('Google profie' , profile)
     // first argument is the error argument and then the profile 
     done(null , profile) // to let passport know that the user is now logged in 
 }
 
 passport.use(new Strategy(authOptions , verifyCallback))
 
+// save the session to cookie
+passport.serializeUser((user , done) => {
+    done(null , user.id)
+})
+// puts the user into the req.user parameter
+// Read the session from the cookie
+passport.deserializeUser((id , done) => {
+    // if you want to fetch the user details from the database you can do it here /
+    // pass the updated user in place of id and it will be available in req.user
+    done(null , id)
+})
+
 const PORT = 3000;
 
 app.use(helmet())
+app.use(cookieSession({
+    name: 'session',
+    maxAge:  20 * 1000,
+    keys: [ config.cookieKey ] // session signed with this secret key
+}))
 app.use(passport.initialize())
+app.use(passport.session())
 app.use(logger)
 
 function checkLoggedIn(req , res , next) {
-    const isLoggedIn = false;
+    console.log('Current user' , req.user)
+    const isLoggedIn = req.isAuthenticated() && req.user;
     if(!isLoggedIn){
         return res.status(401).send('Error : You must login to see the secret')
     }
@@ -47,18 +67,27 @@ function checkLoggedIn(req , res , next) {
 app.get('/' , (req , res) => {
     res.sendFile(path.join(__dirname , 'public' , 'index.html'))
 })
+
+
 app.get('/auth/google' , passport.authenticate('google' , {
     scope: ['email'],
 }))
+
 app.get('/auth/google/callback' , passport.authenticate('google' , {
     failureRedirect: '/failure',
     successRedirect: '/',
-    session: false
+    session: true
 }) , (req , res) => { console.log('Google called us back ') })
+
+
 app.get('/failure' , (req , res) => {
     res.send('Failed to authenticate')
 })
-app.get('/auth/logout' , (req , res) => {})
+
+app.get('/auth/logout' , (req , res) => {
+    req.logout() // removes req.user & terminates any logged in session
+    return res.redirect('/')
+})
 
 app.get('/secret', checkLoggedIn , (req , res) => {
     return res.send('Your Secret code is 52 !')
